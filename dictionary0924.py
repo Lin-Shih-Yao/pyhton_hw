65173935af00736efdfdbf2eff4a873424a353f7
Lin_Shih_Yao <lf21227a@gmail.com>
2021-09-25 21:51:41 +0800

    dictionary_hw_0925

---
+++ dictionary0924.py
@@ -0,0 +1,16 @@
+# user_info = {"first": "abc", "second": "def", "last": "xyz"}
+
+# answer = [[answer for answer in range(10)]for _ in range(10)]
+# print(answer)
+
+# artist = {"first": "Neil", "last": "Young"}
+# full_name = artist["first"] + " " + artist["last"]
+# print(full_name)
+
+# from random import choice
+# food =  choice(["cheese pizza", "quiche", "morning bun", "gummy bear", "tea cake"])
+# bakery_stock = {"almond croissant":12, "toffee cookie": 3, "morning bun": 1, "chocolate chunk cookie": 9, "tea cake": 25}
+# if food in bakery_stock:
+#     print(str(bakery_stock[food]) + " " + "left")
+# else:
+#     print("We don't make that")
